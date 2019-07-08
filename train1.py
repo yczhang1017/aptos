e66f12f96fa9d8fcb2799154a4e785106e274401
yczhang1017 <zhangyuchen12344@gmail.com>
2019-07-08 01:02:21 -0700

    Update train1.py

--- train1.py
+++ train1.py
@@ -192,8 +192,9 @@ def main():
         criterion = nn.MSELoss()
     
     elif args.loss == 'wmse':
-        weight = torch.pow(torch.tensor(dist[0]/dist,dtype=torch.float),0.4)
-        weight[-1]=weight.max()
+        #weight = torch.pow(torch.tensor(dist[0]/dist,dtype=torch.float),0.4)
+        #weight[-1]=weight.max()
+        weight = torch.tensor([1,1.4,1.2,2,3])
         print(weight)
         criterion = weighted_mse(weight)
         
