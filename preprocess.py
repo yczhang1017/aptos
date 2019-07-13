d5098a109018d3a74e5527be0bb5f52a90a9c8ac
yczhang1017 <zhangyuchen12344@gmail.com>
2019-07-13 16:09:25 -0700

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -41,7 +41,7 @@ def crop_image(im, size=512):
     open_cv_image = open_cv_image[:, :, ::-1].copy() 
     gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
     ret,gray = cv2.threshold(gray,10,255,cv2.THRESH_BINARY)
-    im2,contours,hierarchy = cv2.findContours(gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours,hierarchy = cv2.findContours(gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     if not contours:
         print('no contours!')
         flag = 0
