39a007246cb6f4dd303a4cbac664a29c8270ebd1
yczhang1017 <zhangyuchen12344@gmail.com>
2019-07-30 23:24:38 -0700

    Update train1.py

--- train1.py
+++ train1.py
@@ -316,7 +316,6 @@ def main():
     #messidor
     df4=pd.read_csv(
             'IEEE/label/train.csv',header=1,
-            names =['id_code', 'diagnosis'], usecols=[0,1],
             names =['id', 'diagnosis'], usecols=[0,1],
             dtype={'id':str, 'diagnosis':np.int8})
     df4=df4.append(pd.read_csv(
